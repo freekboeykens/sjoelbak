ac65b4d7a8db73562a8116f4bd93e8df9d011226
Freek Boeykens <freek.boeykens@gmail.com>
2019-12-29 10:26:50 +0100

    Add solution

---
+++ Cut-Throat Cricket Sjoelbak Deathmatch Scoreboard/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cut_Throat_Cricket_Sjoelbak_Deathmatch_Scoreboard
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
